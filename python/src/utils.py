3b8700a5a5b756e4d5c2e63a32bc89854a85b8f3
Aditya <60684641+0x0elliot@users.noreply.github.com>
2023-07-24 03:06:57 +0530

    progress: we got get_domain_key_sync to work

--- python/src/utils.py
+++ python/src/utils.py
@@ -5,8 +5,7 @@ from solana.rpc.types import Pubkey as PublicKey
 def get_hashed_name_sync(name: str) -> bytes:
     input_data = (HASH_PREFIX + name).encode("utf-8")
     sha256_hash = hashlib.sha256(input_data).digest()
-    print(sha256_hash[2:])
-    return sha256_hash[2:]
+    return sha256_hash
 
 def get_name_account_key_sync(
     hashed_name: bytes, name_class: PublicKey = None, name_parent: PublicKey = None
@@ -77,5 +76,4 @@ def get_domain_key_sync(domain: str, record: bool = False):
 
 
 if __name__ == "__main__":
-    print(get_domain_key_sync("wallet-guide-5.sol"))
-    print(get_domain_key_sync("wallet-guide-5"))
+    print(get_domain_key_sync("abhinavmir.sol"))
