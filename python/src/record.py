6dd4d967204d8d71fb85989f3bff759e0984d942
Abhinav Srivastava <atg271@gmail.com>
2023-07-26 21:25:38 +0530

    wip

--- python/src/record.py
+++ python/src/record.py
@@ -18,7 +18,6 @@ def get_record_key_sync(domain: str, record: Record) -> str:
     pubkey = get_domain_key_sync(record + "." + domain, True)["pubkey"]
     return pubkey
 
-
 def get_sol_record(connection: Client, domain_pub_key: str, registry: str) -> Pubkey:
     return get_record(connection, domain_pub_key, registry, Record.SOL)
 
