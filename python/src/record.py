ff13004799ed8d1129a2f4b8f404aea0af683ffe
Abhinav Srivastava <atg271@gmail.com>
2023-07-26 05:34:27 +0530

    wip

--- python/src/record.py
+++ python/src/record.py
@@ -4,14 +4,26 @@ from types.record import Record
 from solana.rpc.api import Client
 from solana.rpc.types import Pubkey
 from state import NameRegistryState
+from errors import ErrorType, SNSError
+
 
 def get_record_key_sync(domain: str, record: Record) -> str:
     pubkey = get_domain_key_sync(record + "." + domain, True)["pubkey"]
     return pubkey
 
+
 def get_sol_record(connection: Client, domain_pub_key: str, registry: str) -> Pubkey:
     return get_record(connection, domain_pub_key, registry, Record.SOL)
 
 # always use in try catch
-def get_record(connection: Client, domain: str, record: Record, deserialize: bool = True) -> NameRegistryState:
-    if bool:
+
+
+def get_record(
+        connection: Client, domain: str, record: Record, deserialize: bool = True
+) -> NameRegistryState:
+    public_key = get_record_key_sync(domain=domain, record=record)
+    [registry, nft_owner] = NameRegistryState.retrieve(
+        connection=connection, name_account_key=public_key)
+
+    if not registry.data:
+        SNSError(error_type=ErrorType.NoRecordData)
