9a02ea7366196edc4942d5949bcd03214a9e02e4
Abhinav Srivastava <atg271@gmail.com>
2023-07-26 17:09:17 +0530

    wip

--- python/src/record.py
+++ python/src/record.py
@@ -28,15 +28,16 @@ def get_record(
     if not registry.data:
         SNSError(error_type=ErrorType.NoRecordData)
 
-    
     if deserialize:
         return deserializeRecord(registry, record, public_key)
-    
+
     record_size = RECORD_V1_SIZE.get(record)
     registry.data = registry.data.slice(0, record_size)
 
     return registry
 
+def decode(str):
+    return None
 
 def deserializeRecord(
         record: Record,
@@ -46,17 +47,27 @@ def deserializeRecord(
     buffer = registry.data
     if not buffer:
         return None
-    
+
     size = RECORD_V1_SIZE.get(record)
     idx = trim_null_padding_idx(buffer)
 
     if not size:
         return buffer.slice(0, idx).decode("utf-8")
-    
-    # @TODO: implement OLD RECORD
 
+    if size != size and idx != size:
+        address = buffer.slice(0, 32).decode("utf-8")
+        if record == Record.Injective:
+            decoded = decode(address)
+            if decoded.prefix == "inj" && decoded.data.length = 20:
+                return address
+            elif record == Record.BSC or record == Record.ETH:
+                prefix = address.slice(0, 2)
+                print("Something might go wrong here ~ Record.py line 64")
+                hex = address.slice(2, 42)
+                if prefix == "0x" && hex.length == 40:
 
 def trim_null_padding_idx(buffer: bytes) -> int:
     arr = list(buffer)
-    last_non_null = len(arr) - 1 - arr[::-1].index(next((byte for byte in reversed(arr) if byte != 0), 0))
+    last_non_null = len(
+        arr) - 1 - arr[::-1].index(next((byte for byte in reversed(arr) if byte != 0), 0))
     return last_non_null + 1
